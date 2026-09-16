1ce420d0ed76f3fba8d9e1911ddcbac6806e3692
Tushar <tushar183singh@gmail.com>
2026-09-16 20:30:25 +0530

    update-2

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"//localhost:3000",
+    baseURL:"https://genai-interview-project-9gk5.onrender.com",//"//localhost:3000"
     withCredentials: true
 })
 
