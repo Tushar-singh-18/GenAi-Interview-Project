cef3468d619d476ef70eefc0ab1454eb58508d25
Tushar <tushar183singh@gmail.com>
2026-09-17 20:47:05 +0530

    fix cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "https://gen-ai-delta-eight.vercel.app/" ],
+    origin: ["http://localhost:5173", "https://gen-ai-delta-eight.vercel.app" ],
         credentials: true
 }))
 
